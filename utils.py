bef657dd52fde9ee7397510a8c110c856adaa4d7
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2023-12-01 23:57:02 -0300

    TDD: Good reads validation

--- utils.py
+++ utils.py
@@ -1,2 +1,8 @@
-def is_valid_goodreads_url(url: str):
-    pass
+from validators.url import url as url_validator
+import re
+
+
+def is_valid_goodreads_url(url: str) -> bool:
+    if url_validator(url) and re.match(pattern=r'https?://www\.goodreads\.com/.*', string = url):
+        return True
+    return False
