e197c01a2672b033d6abe877f0b4d805fffda504
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2023-12-02 00:10:57 -0300

    Black formatting

--- utils.py
+++ utils.py
@@ -3,6 +3,16 @@ import re
 
 
 def is_valid_goodreads_url(url: str) -> bool:
-    if url_validator(url) and re.match(pattern=r'https?://www\.goodreads\.com/.*', string = url):
+    """Validates if a URL is a proper URL from the Goodreads website.
+
+    Args:
+        url (str): String representing an URL.
+
+    Returns:
+        bool: True if it's a valid GR URL, False otherwise.
+    """
+    if url_validator(url) and re.match(
+        pattern=r"https?://www\.goodreads\.com/.*", string=url
+    ):
         return True
     return False
