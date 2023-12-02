aab6b9567cc4206c70f8d5522972db5e9e553cb8
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2023-12-02 06:24:40 -0300

    TDD is the cool

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from utils import is_valid_goodreads_url
+from utils import is_valid_goodreads_url, is_goodreads_profile
 import pytest
 
 
@@ -8,8 +8,23 @@ import pytest
         ("https://www.goodreads.com/book/show/12345", True),
         ("https://www.example.com", False),
         ("not a url", False),
-        # Add more test cases as needed
+        ("https://www.goodreads.com/user/show/1", True),
+        ("https://www.goodreads.com/user/show/300", True),
     ],
 )
 def test_goodreads_url(url: str, expected: bool):
     assert is_valid_goodreads_url(url) == expected
+
+
+@pytest.mark.parametrize(
+    "url,expected",
+    [
+        ("https://www.goodreads.com/book/show/12345", False),
+        ("https://www.example.com", False),
+        ("not a url", False),
+        ("https://www.goodreads.com/user/show/1", True),
+        ("https://www.goodreads.com/user/show/300", True),
+    ],
+)
+def test_profile_url(url: str, expected: bool):
+    assert is_goodreads_profile(url) == expected
