fb80471fc50bd0fc7c1f00f7d882bb8389c3e655
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2023-12-25 18:58:58 -0300

    Encapsulation

--- src/scraper/scrape.py
+++ src/scraper/scrape.py
@@ -2,12 +2,27 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from src.utils.utils import process_book, parse_infinite_status, setup_browser
-
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webelement import WebElement
 
 URL = "https://www.goodreads.com/review/list/71341746?shelf=read"
+
+
+def scroll_shelf(infinite_status: WebElement, body: WebElement, browser: WebDriver):
+    current_books, remaining_books = parse_infinite_status(infinite_status)
+    while current_books < remaining_books:
+        # Scroll down
+        body.send_keys(Keys.END)
+        # Get updated status
+        WebDriverWait(browser, 10).until(
+            lambda x: len(x.find_elements(By.CLASS_NAME, "bookalike")) > current_books
+        )
+        infinite_status = WebDriverWait(browser, 5).until(
+            lambda x: x.find_element(By.ID, "infiniteStatus")
+        )
+        current_books, _ = parse_infinite_status(infinite_status)
 
 
 def scrape_shelf(url: str):
@@ -24,20 +39,7 @@ def scrape_shelf(url: str):
     infinite_status = WebDriverWait(browser, 5).until(
         EC.presence_of_element_located((By.ID, "infiniteStatus"))
     )
-    current_books, remaining_books = parse_infinite_status(infinite_status)
-    while current_books < remaining_books:
-        # TODO: This scrolling stuff? That's a function.
-        # Scroll down
-        body.send_keys(Keys.END)
-        # Get updated status
-        WebDriverWait(browser, 10).until(
-            lambda x: len(x.find_elements(By.CLASS_NAME, "bookalike")) > current_books
-        )
-        infinite_status = WebDriverWait(browser, 5).until(
-            lambda x: x.find_element(By.ID, "infiniteStatus")
-        )
-        current_books, _ = parse_infinite_status(infinite_status)
-
+    scroll_shelf(infinite_status, body, browser)
     books = browser.find_elements(By.CLASS_NAME, "bookalike")
     book_list = [process_book(browser, book) for book in books]
     browser.quit()
