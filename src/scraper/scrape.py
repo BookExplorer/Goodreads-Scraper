8c8249878078b47bc7cf5c9033fc66b526ef6755
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2024-01-11 22:38:49 -0300

    Final cleanuo

--- src/scraper/scrape.py
+++ src/scraper/scrape.py
@@ -18,6 +18,13 @@ from typing import Dict, List
 def scroll_shelf(
     infinite_status: WebElement, body: WebElement, browser: WebDriver
 ) -> None:
+    """Scrolls shelf until all books have been loaded.
+
+    Args:
+        infinite_status (WebElement): Element at the bottom of the page that gives the number of books loaded.
+        body (WebElement): Body element of the webpage.
+        browser (WebDriver): Browser being used by Selenium to scrape.
+    """
     current_books, remaining_books = parse_infinite_status(infinite_status)
     while current_books < remaining_books:
         # Scroll down
@@ -33,7 +40,15 @@ def scroll_shelf(
         current_books, _ = parse_infinite_status(infinite_status)
 
 
-def scrape_shelf(url: str):
+def scrape_shelf(url: str) -> List[Dict[str, any]]:
+    """Performs the extraction of all the books from a valid shelf URL.
+
+    Args:
+        url (str): Valid URL for a shelf.
+
+    Returns:
+        List[Dict[str, any]]: List of dictionaries where each is a book extracted from the shelf and processed accordingly.
+    """
     browser = setup_browser()
     browser.get(url)
 
@@ -54,7 +69,20 @@ def scrape_shelf(url: str):
     return book_list
 
 
-def main(user_profile: str) -> List[Dict[str, str]]:
+def process_profile(user_profile: str) -> List[Dict[str, str]]:
+    """Main function for the scraping.
+    Will get a url, validate it as a GR profile and, if valid, create the shelf url to then be scraped.
+    Returns the list of books in that shelf.
+
+    Args:
+        user_profile (str): URL to be scraped, expected to be a valid GR profile.
+
+    Raises:
+        ValueError: If it's not a valid GR profile.
+
+    Returns:
+        List[Dict[str, str]]: List of dictionaries where each is a book extracted from the shelf and processed accordingly.
+    """
     valid_profile = is_goodreads_profile(user_profile)
     if not valid_profile:
         raise ValueError("Your URL was not a valid Goodreads profile.")
@@ -65,5 +93,5 @@ def main(user_profile: str) -> List[Dict[str, str]]:
 
 if __name__ == "__main__":
     user_profile = input("Please input your profile.")
-    books = main(user_profile)
+    books = process_profile(user_profile)
     print(books)
